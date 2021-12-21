b0a42d23be58087de948cb8c00480327714d79db
Morgan Jacquet <jacquetmorganmj@gmail.com>
2021-12-21 15:06:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <!-- Add default assets -->
+    <?=include('configs/assets_css.php');?>
+    <title>Gestion d'élèves</title>
+</head>
+<body>
+    
+    <?=include('configs/assets_js.php');?>
+</body>
+</html>
