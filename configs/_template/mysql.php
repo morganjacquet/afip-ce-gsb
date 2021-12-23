b3abba98b03cb4864d22da4d6c639e5b0b23ad1c
Morgan Jacquet <jacquetmorganmj@gmail.com>
2021-12-23 09:50:41 +0100

    clean for refacto MVC

--- configs/_template/mysql.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $user = '';
-    $pass = '';
-    $host = '127.0.0.1';
-    $bdd = 'gestion_eleves';
-
-    $bdd_connexion = new PDO('mysql:host='.$host.';dbname='.$bdd, $user, $pass);
-?>
--- configs/assets_js.php
+++
@@ -1,2 +0,0 @@
-<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-<script src="assets/js/bootstrap.min.js"></script>
